74463fb9fd474aff2512851c25d15173a7c5486e
Rim Rakhimov <rimrakhimov@gmail.com>
2026-09-15 03:17:19 +0400

    update api to restrict usages to spsc pattern

--- ring-buffer/src/main.rs
+++ ring-buffer/src/main.rs
@@ -1,22 +1,24 @@
-use ring_buffer::SpscRingBuffer;
+use ring_buffer::spsc;
 use std::{thread, thread::sleep, time::Duration};
 
 fn main() {
-    let buffer = SpscRingBuffer::new(2);
+    let (sender, receiver) = spsc::channel(2);
 
-    thread::scope(|scope| {
-        scope.spawn(|| {
-            for i in 0..100 {
-                println!("{}", buffer.try_produce(i));
-                sleep(Duration::from_millis(10));
-            }
-        });
-
-        scope.spawn(|| {
-            for _ in 0..100 {
-                println!("value={:#?}", buffer.try_consume());
-                sleep(Duration::from_millis(10));
-            }
-        });
+    let thread_1 = thread::spawn(move || {
+        // let mut sender = sender;
+        for i in 0..100 {
+            println!("produce({i}) {:?}", sender.try_produce(i));
+            sleep(Duration::from_millis(10));
+        }
     });
+
+    let thread_2 = thread::spawn(move || {
+        for _ in 0..100 {
+            println!("consume value={:#?}", receiver.try_consume());
+            sleep(Duration::from_millis(5));
+        }
+    });
+
+    thread_1.join().unwrap();
+    thread_2.join().unwrap();
 }
--- ring-buffer/tests/miri_ring.rs
+++ ring-buffer/tests/miri_ring.rs
@@ -1,92 +1,125 @@
-use ring_buffer::SpscRingBuffer;
+use ring_buffer::spsc;
 use std::thread;
 use waitgroup::WaitGroup;
 
 #[test]
 fn simple_fifo() {
-    let buffer = SpscRingBuffer::new(5);
+    let (sender, receiver) = spsc::channel(5);
 
-    assert!(buffer.try_produce(1));
-    assert!(buffer.try_produce(2));
+    assert_eq!(Ok(()), sender.try_produce(1));
+    assert_eq!(Ok(()), sender.try_produce(2));
 
-    assert_eq!(Some(1), buffer.try_consume());
-    assert_eq!(Some(2), buffer.try_consume());
-    assert_eq!(None, buffer.try_consume());
+    assert_eq!(Some(1), receiver.try_consume());
+    assert_eq!(Some(2), receiver.try_consume());
+    assert_eq!(None, receiver.try_consume());
 
-    assert!(buffer.try_produce(3));
-    assert_eq!(Some(3), buffer.try_consume());
+    assert_eq!(Ok(()), sender.try_produce(3));
+    assert_eq!(Some(3), receiver.try_consume());
 }
 
 #[test]
 fn full_empty() {
-    let buffer = SpscRingBuffer::new(3);
+    let (sender, receiver) = spsc::channel(3);
 
-    assert_eq!(None, buffer.try_consume());
+    assert_eq!(None, receiver.try_consume());
 
-    assert!(buffer.try_produce(1));
-    assert!(buffer.try_produce(2));
-    assert!(buffer.try_produce(3));
+    assert_eq!(Ok(()), sender.try_produce(1));
+    assert_eq!(Ok(()), sender.try_produce(2));
+    assert_eq!(Ok(()), sender.try_produce(3));
 
-    assert!(!buffer.try_produce(4));
+    assert_eq!(Err(4), sender.try_produce(4));
 
-    assert_eq!(Some(1), buffer.try_consume());
-    assert_eq!(Some(2), buffer.try_consume());
-    assert_eq!(Some(3), buffer.try_consume());
-    assert_eq!(None, buffer.try_consume());
+    assert_eq!(Some(1), receiver.try_consume());
+    assert_eq!(Some(2), receiver.try_consume());
+    assert_eq!(Some(3), receiver.try_consume());
+    assert_eq!(None, receiver.try_consume());
 
-    assert!(buffer.try_produce(4));
-    assert_eq!(Some(4), buffer.try_consume());
+    assert_eq!(Ok(()), sender.try_produce(4));
+    assert_eq!(Some(4), receiver.try_consume());
 }
 
 #[test]
 fn pub_receive() {
-    let buffer = SpscRingBuffer::new(1);
+    let (sender, receiver) = spsc::channel(1);
 
     thread::scope(|scope| {
-        scope.spawn(|| {
-            assert!(buffer.try_produce(42));
+        scope.spawn(move || {
+            assert_eq!(Ok(()), sender.try_produce(42));
         });
 
-        scope.spawn(|| {
-            let value = buffer.try_consume();
-            assert!(value == None || value == Some(42));
+        scope.spawn(move || {
+            let value = receiver.try_consume();
+            assert!(value.is_none() || value == Some(42));
         });
     });
 }
 
 #[test]
 fn slot_reuse() {
-    let buffer = SpscRingBuffer::new(1);
+    let (sender, receiver) = spsc::channel(1);
 
     thread::scope(|scope| {
-        scope.spawn(|| {
+        scope.spawn(move || {
             for i in 0..3 {
-                buffer.try_produce(i);
+                let _ = sender.try_produce(i);
             }
         });
 
-        scope.spawn(|| {
-            buffer.try_consume();
+        scope.spawn(move || {
+            receiver.try_consume();
         });
     })
 }
 
 #[test]
 fn different_slots() {
-    let buffer = SpscRingBuffer::new(2);
+    let (sender, receiver) = spsc::channel(2);
 
     let wait_group = WaitGroup::new();
-    wait_group.add(1);
     thread::scope(|scope| {
-        scope.spawn(|| {
-            assert!(buffer.try_produce(1));
+        let wait_group = &wait_group;
+        wait_group.add(1);
+
+        scope.spawn(move || {
+            assert_eq!(Ok(()), sender.try_produce(1));
             wait_group.done();
-            assert!(buffer.try_produce(2));
+            assert_eq!(Ok(()), sender.try_produce(2));
         });
 
-        scope.spawn(|| {
+        scope.spawn(move || {
             wait_group.wait();
-            assert_eq!(Some(1), buffer.try_consume());
+            assert_eq!(Some(1), receiver.try_consume());
         });
     })
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct NotCopy {
+    v: i32,
+}
+
+#[test]
+fn not_copy() {
+    let (sender, receiver) = spsc::channel(1);
+
+    let wait_group = WaitGroup::new();
+    thread::scope(|scope| {
+        let wait_group = &wait_group;
+        wait_group.add(1);
+
+        scope.spawn(move || {
+            assert_eq!(Ok(()), sender.try_produce(NotCopy { v: 0 }));
+            wait_group.done();
+
+            let mut value = NotCopy { v: 0 };
+            while let Err(retry) = sender.try_produce(value) {
+                value = retry
+            }
+        });
+
+        scope.spawn(move || {
+            wait_group.wait();
+            assert_eq!(Some(NotCopy { v: 0 }), receiver.try_consume());
+        });
+    });
 }
